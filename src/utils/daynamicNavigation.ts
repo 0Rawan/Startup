8b2c0e4388250ab4ddad66d1553429066634cfad
0Rawan <r.k096@hotmail.com>
2024-09-23 16:57:02 +0300

    feat: navbar

---
+++ src/utils/daynamicNavigation.ts
@@ -0,0 +1,23 @@
+/* -------------------------------------------------------------------------- */
+/*                      is children page active checking                      */
+/* -------------------------------------------------------------------------- */
+
+export function isChildrenPageActive() {
+    // if (path && match) {
+    //     if (path === match) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    return false;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                       is parent page active checking                       */
+/* -------------------------------------------------------------------------- */
+export function isParentPageActive() {
+    // if (pages) {
+    //     return pages.some((page) => page.path === path);
+    // }
+    return false;
+}
